d04c83e815879b573cbd0add86337e5fc21bdb8e
Trent Templet <tntemplet@gmail.com>
2024-06-27 16:11:20 -0500

    :add HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { HashRouter as RouterProvider } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
 import { router } from './router.jsx'
 
 
--- src/router.jsx
+++ src/router.jsx
@@ -1,10 +1,10 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import App from "./App";
 import Bracket from "./routes/Bracket";
 import Parking from "./routes/Parking";
 import Rosters from "./routes/Rosters";
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
     {path: "/", element: <App />},
     {path: "/bracket", element: <Bracket /> },
     {path: "/parking", element: <Parking /> },
