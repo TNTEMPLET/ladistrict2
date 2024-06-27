69cef03b946ff78e6968656eb1eb42a18271fbc8
Trent Templet <tntemplet@gmail.com>
2024-06-27 16:08:07 -0500

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { RouterProvider } from 'react-router-dom'
+import { HashRouter as RouterProvider } from 'react-router-dom'
 import { router } from './router.jsx'
 
 
