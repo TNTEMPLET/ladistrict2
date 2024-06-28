88f57983e919f40d010c1eb3f74460101ee873eb
Trent Templet <tntemplet@gmail.com>
2024-06-28 09:18:57 -0500

    :fixed homescreen image

---
+++ src/Components/Rosters/RenderRoster.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RenderRoster = () => {
+    return (<>
+    <div className={styles.rosterContainer}>
+        <img className={styles.roster} src={Minors9} alt="" />
+    </div>
+    </>)
+}
+
+export default RenderRoster
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import logo from './src/images/louisianalllogo.png';
 
 const Home = () => {
   return (<>
   <div className='home-container'>
-    <img src="./src/images/louisianalllogo.png" alt="" />
+    <img src={logo} alt="" />
     <div className="text-info">
       <h1>DISTRICT 2 TOURNAMENT</h1>
       <h1>2024</h1>
