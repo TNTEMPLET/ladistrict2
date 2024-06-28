4b0dfd6f2580a8704ab6ad0594bfae06c5997e6b
Trent Templet <tntemplet@gmail.com>
2024-06-27 23:08:31 -0500

    Fixed :add 9U Image

--- src/Components/Rosters/Minor9.jsx
+++ src/Components/Rosters/Minor9.jsx
@@ -3,7 +3,7 @@ import Minors9 from './District 2 Tournament Minor 9-11 Rosters.png'
 
 const Minor9 = () => {
   return (<>
-    <img src={Minor9} alt="" />
+    <img src={Minors9} alt="" />
   </>)
 }
 
