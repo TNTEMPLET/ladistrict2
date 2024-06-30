10f78694d4bd55bbd3af41c03f0988816797798c
Trent Templet <68797176+TNTEMPLET@users.noreply.github.com>
2024-06-30 13:11:08 -0500

    Update 9MinorsBracket.jsx

--- src/Components/Brackets/9MinorsBracket.jsx
+++ src/Components/Brackets/9MinorsBracket.jsx
@@ -25,8 +25,8 @@ function MinorsBracket_9(){
     const loserOne =  oneTopScore !== oneBottomScore ? (oneTopScore < oneBottomScore ? elevenMinorsTeams.one : elevenMinorsTeams.two) : "A";
     
     //GAME 2
-    const twoTopScore = 0;
-    const twoBottomScore = 0;
+    const twoTopScore = 18;
+    const twoBottomScore = 3;
     const winnTwo = twoTopScore !== twoBottomScore ? (twoTopScore > twoBottomScore ? winnOne : elevenMinorsTeams.three) : "";
     const loserTwo = twoTopScore !== twoBottomScore ? (twoTopScore < twoBottomScore ? winnOne : elevenMinorsTeams.three) : "B";
     
