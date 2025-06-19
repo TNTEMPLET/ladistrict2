74c1358e2d0247b1fa5c0ae9dfb2eb8080849797
Trent Templet <68797176+TNTEMPLET@users.noreply.github.com>
2025-06-19 08:47:18 -0500

    updated homepage to 2025 district 2 tournament

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -9,9 +9,8 @@ const Home = () => {
   <div className={styles.homeContainer}>
     <img src={logo} alt="" className='logo' />
     <div className={styles.textInfo}>
-      <h1>LOUISIANA  STATE TOURNAMENT</h1>
-      <h6>Junior and Senior League</h6>
-      <h1>2024</h1>
+      <h1>LOUISIANA DISTRICT 2 TOURNAMENT</h1>
+      <h1>2025</h1>
       
       <div className={styles.hostInfo}>
         <h5>Hosted By: </h5>
