464ace77501f90f55fa822133b7bda9835fffeaa
Trent Templet <68797176+TNTEMPLET@users.noreply.github.com>
2025-06-29 11:33:37 -0500

    12 G3 EB 10 NO 0

--- src/Components/Brackets/12MajorsBracket.jsx
+++ src/Components/Brackets/12MajorsBracket.jsx
@@ -33,7 +33,7 @@ function MajorBracket_12(){
     const loserTwo = twoTopScore !== twoBottomScore ? (twoTopScore < twoBottomScore ? teams.three : teams.four) : "A";
 
     //GAME 3
-    const threeTopScore = 0;
+    const threeTopScore = 10;
     const threeBottomScore = 0;
     const winnThree = threeTopScore !== threeBottomScore ? (threeTopScore > threeBottomScore ? winnOne : teams.five) : "";
     const loserThree =  threeTopScore !== threeBottomScore ? (threeTopScore < threeBottomScore ? winnOne : teams.five) : "C";
