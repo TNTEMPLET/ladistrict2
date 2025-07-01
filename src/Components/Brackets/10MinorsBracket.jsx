f227743cf7ae32ef09f4fb742113561178eda0ee
Trent Templet <68797176+TNTEMPLET@users.noreply.github.com>
2025-06-30 19:12:24 -0500

    10 APN 8 APR 1

--- src/Components/Brackets/10MinorsBracket.jsx
+++ src/Components/Brackets/10MinorsBracket.jsx
@@ -43,8 +43,8 @@ function MinorsBracket_10(){
     const winnFour =  fourTopScore!== fourBottomScore ? (fourTopScore > fourBottomScore ? loserOne : loserTwo) :"";
 
     //GAME 5
-    const fiveTopScore = 0;
-    const fiveBottomScore = 0;
+    const fiveTopScore = 1;
+    const fiveBottomScore = 8;
     const winnFive =  fiveTopScore !== fiveBottomScore ? (fiveTopScore > fiveBottomScore ? winnTwo : winnThree) :"";
     const loserFive =  fiveTopScore !== fiveBottomScore ? (fiveTopScore < fiveBottomScore ? winnTwo : winnThree) :"D";
 
